88838bfb0e103109c64f99779b04a7b0a9f0691f
MateusSilvaSPTECH <mateus.ssouza@sptech.school>
2026-05-24 23:22:45 -0300

    Feat: Atualizar senha usuario

---
+++ src/main/java/school/sptech/cursos/DTO/Usuario/ResetSenhaNovaRequest.java
@@ -0,0 +1,12 @@
+package school.sptech.cursos.DTO.Usuario;
+
+public class ResetSenhaNovaRequest {
+    private String novaSenha;
+    public String getNovaSenha() {
+        return novaSenha;
+    }
+
+    public void setNovaSenha(String novaSenha) {
+        this.novaSenha = novaSenha;
+    }
+}
---
+++ src/main/java/school/sptech/cursos/DTO/Usuario/ResetSenhaRequest.java
@@ -0,0 +1,15 @@
+package school.sptech.cursos.DTO.Usuario;
+
+public class ResetSenhaRequest {
+    private String senhaAtual;
+
+    public String getSenhaAtual() {
+        return senhaAtual;
+    }
+
+    public void setSenhaAtual(String senhaAtual) {
+        this.senhaAtual = senhaAtual;
+    }
+
+}
+
